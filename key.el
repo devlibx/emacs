c65462b7b75ed5aab4dd1f4445c8656454abb4f2
Harish Bohara <harish.bohara@gmail.com>
2023-12-07 13:16:31 +0530

    Shortcut for fond refrences

--- key.el
+++ key.el
@@ -2,3 +2,6 @@
 ;; Meta-Up down can go to next/preious paragraph
 (global-set-key (kbd "ESC <up>") 'backward-paragraph)
 (global-set-key (kbd "ESC <down>") 'forward-paragraph)
+
+;; lsp-find-references - Meta-Shift (. or >)
+(global-set-key (kbd "M->") 'lsp-find-references)
