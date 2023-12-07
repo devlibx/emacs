491583e23f250be80f09d5d3ea38ca483ba842a6
Harish Bohara <harish.bohara@gmail.com>
2023-12-07 13:19:36 +0530

    Added help in readme

--- key.el
+++ key.el
@@ -4,4 +4,6 @@
 (global-set-key (kbd "ESC <down>") 'forward-paragraph)
 
 ;; lsp-find-references - Meta-Shift (. or >)
+;; Use M-. to find the source of the symobl. If you want to find all the
+;; places wehre this symbol is used then used this
 (global-set-key (kbd "M->") 'lsp-find-references)
