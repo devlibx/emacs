d58640344ba2c8c58c31447f411671b5dbdba8eb
Harish Bohara <harish.bohara@gmail.com>
2023-12-14 01:33:40 +0530

    Added greenyellow to org-level-

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-level-1 ((t (:inherit outline-1 :extend nil :background "gray34" :foreground "greenyellow" :overline "coral" :weight heavy)))))
 
 ;; Location to load packages
 (require 'package)
