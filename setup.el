8c7964c09618a446f746191fd6ea15453ebb241c
Harish Bohara <harish.bohara@gmail.com>
2024-09-16 15:19:07 +0530

    Disable backup files

--- setup.el
+++ setup.el
@@ -1,4 +1,7 @@
 
+;; Disable making backup file
+(setq make-backup-files nil)
+
 ;; Install Magit
 (use-package magit
   :ensure t)
@@ -8,3 +11,12 @@
   :ensure t
   :config
   (global-set-key (kbd "C-x l") 'mc/edit-lines))
+
+
+;; Setup projectile
+;; Setq my default project location in search path
+(use-package projectile
+  :ensure t
+  :config
+  (setq projectile-project-search-path
+	'("/Users/harishbohara/workplace/personal" "/Users/harishbohara/workplace")))
