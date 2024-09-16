2af28b0b6dd99146473341f9d0c080ad4a9f0810
Harish Bohara <harish.bohara@gmail.com>
2024-09-16 20:04:19 +0530

    Added support to run rust project from workspcae

--- lang/rust.el
+++ lang/rust.el
@@ -22,3 +22,17 @@
   (add-hook 'rust-mode-hook 'cargo-minor-mode)
   )
 
+
+;; Setup cargo command to run this workspace
+;; This will find all the modules from the workspace and allow us to pick one
+(require 'cargo)
+(defun my-cargo-run-package ()
+  "List and run a specific Cargo package from the workspace."
+  (interactive)
+  (let* ((default-directory (locate-dominating-file default-directory "Cargo.toml"))
+         (packages (split-string
+                    (shell-command-to-string "cargo metadata --no-deps --format-version 1 | jq -r '.packages[].name'")
+                    "\n" t))
+         (package (completing-read "Select package: " packages)))
+    (compile (concat "cargo run --package " package))))
+
