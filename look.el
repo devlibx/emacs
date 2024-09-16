7ce49574670904c792142516c1a5c85b648cd3b6
Harish Bohara <harish.bohara@gmail.com>
2024-09-16 15:33:00 +0530

    Clean-up

--- look.el
+++ look.el
@@ -1,4 +1,13 @@
 
+;; Disable the startup screen
+(setq inhibit-startup-message t)
+
+;; Optionally, disable the initial scratch message
+(setq initial-scratch-message nil)
+
+;; For Emacs 28 or later, disable the welcome screen
+(setq inhibit-startup-echo-area-message t)
+
 ;; Some simple configs
 (global-display-line-numbers-mode t) ;; Enable line number
 
