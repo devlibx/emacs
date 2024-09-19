88de6692a66034c363759d37f1c9a0a0ac03e4ec
Harish Bohara <harish.bohara@gmail.com>
2024-09-20 00:54:17 +0530

    Added error in flycheck to popup

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(flycheck-error ((t (:background "lightsteelblue1" :foreground "tomato" :underline (:color "#D70000" :style wave :position nil))))))
 
 
 ;; Initialize package sources
