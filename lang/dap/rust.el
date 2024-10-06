a9dda703ef1325bbba943607dcadb3c3731792f0
Harish Bohara <harish.bohara@gmail.com>
2024-10-06 18:36:33 +0530

    remoev rust target

--- lang/dap/rust.el
+++ lang/dap/rust.el
@@ -8,20 +8,4 @@
   '(progn
      (setq dap-debug-verbose t)
      (setq dap-debug-log-level 'debug)
-
-     (dap-register-debug-template "Rust - devlibx-rust-http"
-                                  (list :type "lldb-vscode"					
-                                        :request "launch"
-					:cwd (projectile-project-root)
-                                        :program (expand-file-name "target/debug/devlibx-rust-http" default-directory)
-					:preLaunchTask "cargo build"
-                                        :name "LLDB::Run"))
-     (dap-register-debug-template "Rust - devlibx-rust-base"
-                                  (list :type "lldb-vscode"					
-                                        :request "launch"
-					:cwd (projectile-project-root)
-                                        :program (expand-file-name "target/debug/devlibx-rust-base" default-directory)
-					:preLaunchTask "cargo build"
-                                        :name "LLDB::Run"))
-
      ))
