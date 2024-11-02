0b685242e8d19687aae437e402d180f5eb8f9c64
Harish Bohara <harish.bohara@gmail.com>
2024-11-02 22:42:23 +0530

    [*] incremental change

--- lang/rust.el
+++ lang/rust.el
@@ -13,7 +13,13 @@
 
   ;; File format was not working on save - did following to fix it (29 Sept 2024)
   (setq rustic-format-trigger 'on-save)
-  (setq rustic-format-display-method 'display-buffer)
+
+  ;; When restic formats the buffer, it shows what the results in a new buffer - for now I do not
+  ;; want to display the results - I am ok just to format
+  ;; setting it nil - to see result set 'display-buffer
+  ;; (setq rustic-format-display-method 'display-buffer)
+  (setq rustic-format-display-method nil)
+
   (setq lsp-rust-analyzer-cargo-watch-command "clippy")
   )
 
